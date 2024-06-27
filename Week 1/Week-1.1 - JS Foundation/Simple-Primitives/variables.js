238b807d3e4ce48d4c7290e11516145a61b5e2de
rajneeshkumar15 <rk1441933@gmail.com>
2024-06-27 23:47:48 +0530

    js variables

--- Week 1/Week-1.1 - JS Foundation/Simple-Primitives/variables.js
+++ Week 1/Week-1.1 - JS Foundation/Simple-Primitives/variables.js
@@ -1,5 +1,5 @@
 let num = 5;  // can be updated but not re-declared within the same scope
-var greet = "Hi"; // same as let, but can re-declared within the same scope
+var greet = "Hi !"; // same as let, but can re-declared within the same scope
 const pincode = 246746; // cannot be update nor re-declared
 
 console.log(num);
