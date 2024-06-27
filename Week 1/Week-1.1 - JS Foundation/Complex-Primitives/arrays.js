c85532fc995f9e55fd269e4e6d709105e342412e
rajneeshkumar15 <rk1441933@gmail.com>
2024-06-27 23:55:42 +0530

    arrays in js

---
+++ Week 1/Week-1.1 - JS Foundation/Complex-Primitives/arrays.js
@@ -0,0 +1,14 @@
+// array definition
+const personArray = ["rajneesh", "somi", "xyz"];
+
+// printing array element
+console.log(personArray[0]);
+console.log(personArray[1]);
+console.log(personArray[2]);
+
+console.log();
+
+// much simple-way to print using loop
+for(let i=0;i<personArray.length;i++){
+    console.log(personArray[i]);
+}
